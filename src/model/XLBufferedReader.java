3d49f278ba9e24ab173a8e82cdfb44f5e4b4f59a
dic14ath <dic14ath@RAVEL-12.student.lth.se>
2015-10-16 11:31:02 +0200

    Fishes

---
+++ src/model/XLBufferedReader.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+
+import util.XLException;
+
+//TODO move to another package
+public class XLBufferedReader extends BufferedReader {
+    public XLBufferedReader(String name) throws FileNotFoundException {
+        super(new FileReader(name));
+    }
+
+    // TODO Change Object to something appropriate
+    public void load(Sheet sheet) {
+        try {
+            while (ready()) {
+                String string = readLine();
+                int i = string.indexOf('=');
+                
+                sheet.setSlot(
+                		new Address(string.substring(0, i)),
+                		string.substring(i + 1));
+            }
+        } catch (Exception e) {
+            throw new XLException(e.getMessage());
+        }
+    }
+}
---
+++ src/model/XLBufferedReader2.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+
+import util.XLException;
+
+//TODO move to another package
+public class XLBufferedReader2 extends BufferedReader {
+    public XLBufferedReader2(String name) throws FileNotFoundException {
+        super(new FileReader(name));
+    }
+
+    // TODO Change Object to something appropriate
+    public void load(Map<Address, Slot> map) {
+        try {
+        	SlotCreator slotCreator = new SlotCreator();
+
+            while (ready()) {
+                String string = readLine();
+                int i = string.indexOf('=');
+                
+                map.put(
+                		new Address(
+                				string.substring(0, i)),
+                		slotCreator.createSlot(
+                				string.substring(i + 1)));
+            }
+        } catch (Exception e) {
+            throw new XLException(e.getMessage());
+        }
+    }
+}
